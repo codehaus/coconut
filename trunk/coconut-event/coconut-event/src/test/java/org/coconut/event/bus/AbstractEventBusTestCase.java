8fabfd1ececd4b6e04d43bb0324158224c4f1558
kasper <kasper@0b154b62-a015-0410-9c24-a35e082c6f94>
2006-09-28 16:53:18 +0000

    update coconut event

---
+++ trunk/coconut-event/coconut-event/src/test/java/org/coconut/event/bus/AbstractEventBusTestCase.java
@@ -0,0 +1,32 @@
+package org.coconut.event.bus;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import junit.framework.AssertionFailedError;
+
+import org.coconut.core.Offerable;
+import org.coconut.test.MockTestCase;
+
+/**
+ * @author <a href="mailto:kasper@codehaus.org">Kasper Nielsen </a>
+ */
+public class AbstractEventBusTestCase extends MockTestCase {
+
+    public Offerable createQueueOfferableOnce(final BlockingQueue<Object> queue) {
+        return new Offerable() {
+            private AtomicBoolean hasBeenCalled = new AtomicBoolean();
+
+            public boolean offer(Object o) {
+                if (hasBeenCalled.get()) {
+                    (new Exception()).printStackTrace();
+                    throw new AssertionFailedError(
+                            "createQueueOfferableOnce called twice");
+                }
+                hasBeenCalled.getAndSet(true);
+                queue.add(o);
+                return true;
+            }
+        };
+    }
+}
