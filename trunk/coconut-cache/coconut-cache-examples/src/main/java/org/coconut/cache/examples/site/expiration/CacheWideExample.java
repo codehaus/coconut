53e43f85290cc1509bcf89ec3486f2322ce67340
kasper <kasper@0b154b62-a015-0410-9c24-a35e082c6f94>
2006-10-11 12:36:41 +0000

    Adding coconut-cache examples

---
+++ trunk/coconut-cache/coconut-cache-examples/src/main/java/org/coconut/cache/examples/site/expiration/CacheWideExample.java
@@ -0,0 +1,23 @@
+/* Copyright 2004 - 2006 Kasper Nielsen <kasper@codehaus.org> Licensed under 
+ * the MIT license, see http://coconut.codehaus.org/license.
+ */
+package org.coconut.cache.examples.site.expiration;
+
+import java.util.concurrent.TimeUnit;
+
+import org.coconut.cache.Cache;
+import org.coconut.cache.CacheConfiguration;
+import org.coconut.cache.defaults.memory.UnlimitedCache;
+
+/**
+ * @author <a href="mailto:kasper@codehaus.org">Kasper Nielsen</a>
+ * @version $Id: Cache.java,v 1.2 2005/04/27 15:49:16 kasper Exp $
+ */
+public class CacheWideExample {
+    public static void main(String[] args) {
+        CacheConfiguration<String, String> cc = CacheConfiguration.newConf();
+        cc.expiration().setDefaultTimeout(24 * 60 * 60, TimeUnit.SECONDS);
+        Cache<String, String> cache = cc.newInstance(UnlimitedCache.class);
+        cache.put("key", "value"); // element will expire after 24 hours
+    }
+}
