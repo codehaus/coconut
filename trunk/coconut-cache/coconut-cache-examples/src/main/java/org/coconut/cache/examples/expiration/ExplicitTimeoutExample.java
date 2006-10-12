83f3925f67366740f8174486f79ed2e7459d2f92
kasper <kasper@0b154b62-a015-0410-9c24-a35e082c6f94>
2006-10-12 12:52:25 +0000

    adding more examples

--- trunk/coconut-cache/coconut-cache-examples/src/main/java/org/coconut/cache/examples/expiration/ExplicitTimeoutExample.java
+++ trunk/coconut-cache/coconut-cache-examples/src/main/java/org/coconut/cache/examples/expiration/ExplicitTimeoutExample.java
@@ -12,7 +12,7 @@ import org.coconut.cache.defaults.memory.UnlimitedCache;
  * @author <a href="mailto:kasper@codehaus.org">Kasper Nielsen</a>
  * @version $Id: Cache.java,v 1.2 2005/04/27 15:49:16 kasper Exp $
  */
-public class ExplicitTimeoutSetting {
+public class ExplicitTimeoutExample {
     public static void main(String[] args) {
         // START SNIPPET: class
         Cache<String, String> cache = new UnlimitedCache<String, String>();
