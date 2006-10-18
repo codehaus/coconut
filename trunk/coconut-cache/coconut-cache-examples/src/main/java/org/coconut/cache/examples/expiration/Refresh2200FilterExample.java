73363bc430fb51cd5b93c8e39d19749a699532df
kasper <kasper@0b154b62-a015-0410-9c24-a35e082c6f94>
2006-10-18 22:46:02 +0000

    Updating cache examples

--- trunk/coconut-cache/coconut-cache-examples/src/main/java/org/coconut/cache/examples/expiration/Refresh2200FilterExample.java
+++ trunk/coconut-cache/coconut-cache-examples/src/main/java/org/coconut/cache/examples/expiration/Refresh2200FilterExample.java
@@ -14,7 +14,7 @@ import org.coconut.filter.Filter;
  * @author <a href="mailto:kasper@codehaus.org">Kasper Nielsen</a>
  * @version $Id: Cache.java,v 1.2 2005/04/27 15:49:16 kasper Exp $
  */
-public class Refresh2200Filter<K, V> implements Filter<CacheEntry<K, V>> {
+public class Refresh2200FilterExample<K, V> implements Filter<CacheEntry<K, V>> {
     private volatile long nextRefresh;
 
     private volatile long current;
