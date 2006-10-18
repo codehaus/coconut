cf6aa473a39f6b8732510a444283683ab51a2f16
kasper <kasper@0b154b62-a015-0410-9c24-a35e082c6f94>
2006-10-18 22:41:29 +0000

    Updating cache examples

---
+++ trunk/coconut-cache/coconut-cache-examples/src/main/java/org/coconut/cache/examples/expiration/Refresh2200Filter.java
@@ -0,0 +1,35 @@
+/* Copyright 2004 - 2006 Kasper Nielsen <kasper@codehaus.org> Licensed under 
+ * the MIT license, see http://coconut.codehaus.org/license.
+ */
+package org.coconut.cache.examples.expiration;
+
+import java.util.Calendar;
+
+import org.coconut.cache.CacheEntry;
+import org.coconut.filter.Filter;
+
+/**
+ * 
+ * 
+ * @author <a href="mailto:kasper@codehaus.org">Kasper Nielsen</a>
+ * @version $Id: Cache.java,v 1.2 2005/04/27 15:49:16 kasper Exp $
+ */
+public class Refresh2200Filter<K, V> implements Filter<CacheEntry<K, V>> {
+    private volatile long nextRefresh;
+
+    private volatile long current;
+
+    public boolean accept(CacheEntry<K, V> entry) {
+        long now = System.currentTimeMillis();
+        if (now > nextRefresh) {
+            Calendar c = Calendar.getInstance();
+            c.add(Calendar.DAY_OF_MONTH, 1);
+            c.set(Calendar.HOUR_OF_DAY, 22);
+            c.set(Calendar.MINUTE, 0);
+            c.set(Calendar.SECOND, 0);
+            current = nextRefresh;
+            nextRefresh = c.getTimeInMillis();
+        }
+        return current > entry.getLastUpdateTime();
+    }
+}
