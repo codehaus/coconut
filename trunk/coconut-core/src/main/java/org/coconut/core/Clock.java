cb3383cb5840fe84256f0e5b0855207e966cf136
kasper <kasper@0b154b62-a015-0410-9c24-a35e082c6f94>
2007-01-24 12:35:04 +0000

    Updating clock

---
+++ trunk/coconut-core/src/main/java/org/coconut/core/Clock.java
@@ -0,0 +1,81 @@
+/* Copyright 2004 - 2006 Kasper Nielsen <kasper@codehaus.org> Licensed under 
+ * the MIT license, see http://coconut.codehaus.org/license.
+ */
+package org.coconut.core;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author <a href="mailto:kasper@codehaus.org">Kasper Nielsen</a>
+ * @version $Id: Cache.java,v 1.2 2005/04/27 15:49:16 kasper Exp $
+ */
+public abstract class Clock {
+
+    public static final Clock DEFAULT_CLOCK = new Clock() {
+        /** serialVersionUID */
+
+        public long timestamp() {
+            return System.currentTimeMillis();
+        }
+
+        public long relativeTime() {
+            return System.nanoTime();
+        }
+    };
+    public static class DeterministicClock extends Clock {
+
+        /** serialVersionUID */
+        private static final long serialVersionUID = -7600850757932509321L;
+
+        private final AtomicLong absolutTime = new AtomicLong();
+
+        private final AtomicLong relativeTime = new AtomicLong();
+
+        @Override
+        public long timestamp() {
+            return absolutTime.get();
+        }
+
+        public void incrementTimestamp() {
+            absolutTime.incrementAndGet();
+        }
+
+        public void incrementRelativeTime() {
+            relativeTime.incrementAndGet();
+        }
+
+        public void incrementRelativeTime(int amount) {
+            relativeTime.addAndGet(amount);
+        }
+        public void incrementAbsolutTime(int amount) {
+            absolutTime.addAndGet(amount);
+        }
+        @Override
+        public long relativeTime() {
+            return relativeTime.get();
+        }
+
+        public void setTimestamp(long amount) {
+            absolutTime.set(amount);
+        }
+
+        public void setRelativeTime(long amount) {
+            relativeTime.set(amount);
+        }
+    }
+    public abstract long timestamp();
+
+    public abstract long relativeTime();
+
+    public boolean isPassed(long timestamp) {
+        return timestamp() >= timestamp;
+    }
+
+    public long getDeadlineFromNow(long timeout, TimeUnit unit) {
+        return getDeadlineFromNow(unit.toMillis(timeout));
+    }
+    public long getDeadlineFromNow(long timeoutMS) {
+        return timestamp() + timeoutMS;
+    }
+}
