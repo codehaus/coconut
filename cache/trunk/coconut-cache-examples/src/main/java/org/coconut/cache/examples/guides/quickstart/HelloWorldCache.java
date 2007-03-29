0af65cbea62cdb5c7c4ceda27d871fe6cf0670bd
kasper <kasper@0b154b62-a015-0410-9c24-a35e082c6f94>
2007-03-29 19:31:39 +0000

    updating example

--- cache/trunk/coconut-cache-examples/src/main/java/org/coconut/cache/examples/guides/quickstart/HelloWorldCache.java
+++ cache/trunk/coconut-cache-examples/src/main/java/org/coconut/cache/examples/guides/quickstart/HelloWorldCache.java
@@ -1,16 +1,17 @@
 /* Copyright 2004 - 2007 Kasper Nielsen <kasper@codehaus.org> Licensed under 
  * the Apache 2.0 License, see http://coconut.codehaus.org/license.
  */
+// START SNIPPET: helloworld
 package org.coconut.cache.examples.guides.quickstart;
 
 import org.coconut.cache.Cache;
 import org.coconut.cache.defaults.UnsynchronizedCache;
 
 public class HelloWorldCache {
-
     public static void main(String[] args) {
         Cache<Integer, String> cache = new UnsynchronizedCache<Integer, String>();
         cache.put(5, "helloworld");
         System.out.println(cache.get(5)); // prints helloworld
     }
 }
+//END SNIPPET: helloworld
