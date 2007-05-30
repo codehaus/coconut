b2377a16f50ca203e99048267c1a44a07c37b6ad
kasper <kasper@0b154b62-a015-0410-9c24-a35e082c6f94>
2007-05-30 17:24:21 +0000

    adding getAllServices() to cache interface

--- cache/trunk/coconut-cache-api/src/main/java/org/coconut/cache/Cache.java
+++ cache/trunk/coconut-cache-api/src/main/java/org/coconut/cache/Cache.java
@@ -10,44 +10,42 @@ import java.util.concurrent.ConcurrentMap;
 import org.coconut.core.AttributeMap;
 
 /**
- * A <tt>cache</tt> is a collection of data duplicating original values stored
- * elsewhere or computed earlier, where the original data are expensive (usually
- * in terms of access time) to fetch or compute relative to reading from the
- * cache. Once the data are stored in the cache, future use can be made by
- * accessing the cached copy rather than refetching or recomputing the original
- * data, so that the average access time is lowered.
+ * A <tt>cache</tt> is a collection of data duplicating original values stored elsewhere
+ * or computed earlier, where the original data are expensive (usually in terms of access
+ * time) to fetch or compute relative to reading from the cache. Once the data are stored
+ * in the cache, future use can be made by accessing the cached copy rather than
+ * refetching or recomputing the original data, so that the average access time is
+ * lowered.
  * <p>
- * Currently only two implementations exist. Both of them are entirely held in
- * memory: {@link org.coconut.cache.defaults.UnsynchronizedCache} and
+ * Currently only two implementations exist. Both of them are entirely held in memory:
+ * {@link org.coconut.cache.defaults.UnsynchronizedCache} and
  * {@link org.coconut.cache.defaults.SynchronizedCache}.
  * <p>
- * The three collection views, which allow a cache's contents to be viewed as a
- * set of keys, collection of values, or set of key-value mappings only shows
- * values contained in the actual cache not any values that is stored in any
- * CacheLoader. Furthermore, the cache will <tt>not</tt> attempt to fetch
- * updated values for entries that has expired when calling methods on any of
- * the collection views.
+ * The three collection views, which allow a cache's contents to be viewed as a set of
+ * keys, collection of values, or set of key-value mappings only shows values contained in
+ * the actual cache not any values that is stored in any CacheLoader. Furthermore, the
+ * cache will <tt>not</tt> attempt to fetch updated values for entries that has expired
+ * when calling methods on any of the collection views.
  * <p>
- * All general-purpose <tt>Cache</tt> implementation classes should provide
- * two "standard" constructors: a void (no arguments) constructor, which creates
- * an empty cache with default settings, and a constructor with a single
- * argument of type {@link CacheConfiguration}. There is no way to enforce this
- * recommendation (as interfaces cannot contain constructors) but all of the
- * general-purpose cache implementations in Coconut Cache comply. Unlike the
- * java.util.Map which this class extends. A constructor taking a single map is
- * not required.
+ * All general-purpose <tt>Cache</tt> implementation classes should provide two
+ * "standard" constructors: a void (no arguments) constructor, which creates an empty
+ * cache with default settings, and a constructor with a single argument of type
+ * {@link CacheConfiguration}. There is no way to enforce this recommendation (as
+ * interfaces cannot contain constructors) but all of the general-purpose cache
+ * implementations in Coconut Cache comply. Unlike the java.util.Map which this class
+ * extends. A constructor taking a single map is not required.
  * <p>
  * Cache implementations generally do not define element-based versions of the
  * <tt>equals</tt> and <tt>hashCode</tt> methods, but instead inherit the
- * identity-based versions from class <tt>Object</tt>. Nore, are they
- * generally serializable.
+ * identity-based versions from class <tt>Object</tt>. Nore, are they generally
+ * serializable.
  * <p>
- * Unlike {@link java.util.HashMap}, a <tt>cache</tt> does NOT allow
- * <tt>null</tt> to be used as a key or value. It is the authors belief that
- * allowing null values (or keys) does more harm then good, by masking what are
- * almost always usage errors. If nulls are absolutely needed the <a
- * href="http://today.java.net/today/2004/12/10/refactor.pdf">Null Object
- * Pattern</a> can be used as an alternative.
+ * Unlike {@link java.util.HashMap}, a <tt>cache</tt> does NOT allow <tt>null</tt> to
+ * be used as a key or value. It is the authors belief that allowing null values (or keys)
+ * does more harm then good, by masking what are almost always usage errors. If nulls are
+ * absolutely needed the <a
+ * href="http://today.java.net/today/2004/12/10/refactor.pdf">Null Object Pattern</a> can
+ * be used as an alternative.
  * 
  * @author <a href="mailto:kasper@codehaus.org">Kasper Nielsen</a>
  * @version $Id: Cache.java 130 2006-10-11 13:00:46Z kasper $
@@ -58,95 +56,83 @@ import org.coconut.core.AttributeMap;
  */
 public interface Cache<K, V> extends ConcurrentMap<K, V> {
 
-	/**
-     * Returns the name of the cache. If no name has been specified while
-     * constructing the cache a random name should be generated.
-     * 
-     * @return the name of the cache
-     */
-	String getName();
-
-	/**
-     * Returns the current capacity of this cache. If the current capacity of
-     * the is greater then Long.MAX_VALUE, this method returns Long.MAX_VALUE.
-     * 
-     * @return the current capacity of this cache
-     */
-	long getCapacity();
-
-	/**
-     * Performs cleanup of the cache. This might be everything from persisting
-     * stale data to disk to adapting the cache with a better eviction policy
-     * given the current access pattern. This is done to avoid paying the cost
-     * upfront by application threads when accessing entries in the cache
-     * through {@link #get(Object)} or {@link #getAll(Collection)}.
+    /**
+     * Performs cleanup of the cache. This might be everything from persisting stale data
+     * to disk to adapting the cache with a better eviction policy given the current
+     * access pattern. This is done to avoid paying the cost upfront by application
+     * threads when accessing entries in the cache through {@link #get(Object)} or
+     * {@link #getAll(Collection)}.
      * <p>
-     * Unless otherwise specified calling this method is the responsibility of
-     * the user. The typical usage is to create a single thread that
-     * periodically runs this method.
+     * Unless otherwise specified calling this method is the responsibility of the user.
+     * The typical usage is to create a single thread that periodically runs this method.
      * <p>
-     * Implementations that block (stop-the-world) all other concurrent access
-     * to the cache by calling this method should clearly specified it.
-     * 
+     * Implementations that block (stop-the-world) all other concurrent access to the
+     * cache by calling this method should clearly specified it.
      */
-	void evict();
+    void evict();
 
-	/**
-     * Works as {@link java.util.Map#get(Object)} with the following
-     * modification.
+    /**
+     * Works as {@link java.util.Map#get(Object)} with the following modification.
      * <p>
-     * If no mapping exists for the specified key and the cache has a backend
-     * cache or a cache loader. The cache will transparently attempt to load a
-     * value for the specified key through the backend.
+     * If no mapping exists for the specified key and the cache has a backend cache or a
+     * cache loader. The cache will transparently attempt to load a value for the
+     * specified key through the backend.
      * 
      * @param key
      *            key whose associated value is to be returned.
      * @return the value to which this map maps the specified key
      * @throws ClassCastException
-     *             if the key is of an inappropriate type for this map
-     *             (optional).
+     *             if the key is of an inappropriate type for this map (optional).
      * @throws NullPointerException
      *             if the key is <tt>null</tt>
      * @throws CacheException
-     *             if the backend store failed while trying to load a value
-     *             (optional). An implementation might choose to return null
-     *             instead
+     *             if the backend store failed while trying to load a value (optional). An
+     *             implementation might choose to return null instead
      * @see Map#get(Object)
      */
-	V get(Object key);
+    V get(Object key);
 
-	/**
-     * Attempts to retrieve all of the mappings for the specified collection of
-     * keys. The effect of this call is equivalent to that of calling
-     * {@link #get(Object)} on this cache once for each key in the specified
-     * collection. However, in some cases it can be much faster to load several
-     * cache items at once, for example, if the cache must fetch the values from
-     * a remote host.
+    /**
+     * Attempts to retrieve all of the mappings for the specified collection of keys. The
+     * effect of this call is equivalent to that of calling {@link #get(Object)} on this
+     * cache once for each key in the specified collection. However, in some cases it can
+     * be much faster to load several cache items at once, for example, if the cache must
+     * fetch the values from a remote host.
      * <p>
-     * If a value is not contained in the cache and the value cannot be loaded
-     * by any of the configured cache backends. The returned map will contain a
-     * mapping from the key to <tt>null</tt>.
+     * If a value is not contained in the cache and the value cannot be loaded by any of
+     * the configured cache backends. The returned map will contain a mapping from the key
+     * to <tt>null</tt>.
      * <p>
-     * The behavior of this operation is unspecified if the specified collection
-     * is modified while the operation is in progress.
+     * The behavior of this operation is unspecified if the specified collection is
+     * modified while the operation is in progress.
      * 
      * @param keys
      *            a collection of keys whose associated values are to be returned.
-     * @return a map with mappings from each key to the corresponding value, or
-     *         to <tt>null</tt> if no mapping for this key exists.
+     * @return a map with mappings from each key to the corresponding value, or to
+     *         <tt>null</tt> if no mapping for this key exists.
      * @throws ClassCastException
-     *             if any of the keys in the specified collection are of an
-     *             inappropriate type for this cache (optional).
+     *             if any of the keys in the specified collection are of an inappropriate
+     *             type for this cache (optional).
      * @throws NullPointerException
-     *             if the specified collection of keys is <tt>null</tt> or the
-     *             specified collection contains <tt>null</tt>
+     *             if the specified collection of keys is <tt>null</tt> or the specified
+     *             collection contains <tt>null</tt>
      */
-	Map<K, V> getAll(Collection<? extends K> keys);
+    Map<K, V> getAll(Collection<? extends K> keys);
 
-	/**
-     * Retrieves a {@link CacheEntry} for the specified key (optional). If no
-     * entry exists for the specified key any configured cache backend is asked
-     * to try and fetch an entry for the key.
+    Map<Class<?>, Object> getAllServices();
+
+    /**
+     * Returns the current capacity of this cache. If the current capacity of the is
+     * greater then Long.MAX_VALUE, this method returns Long.MAX_VALUE.
+     * 
+     * @return the current capacity of this cache
+     */
+    long getCapacity();
+
+    /**
+     * Retrieves a {@link CacheEntry} for the specified key (optional). If no entry exists
+     * for the specified key any configured cache backend is asked to try and fetch an
+     * entry for the key.
      * <p>
      * 
      * @param key
@@ -154,27 +140,23 @@ public interface Cache<K, V> extends ConcurrentMap<K, V> {
      * @return the cache entry to which this cache maps the specified key, or
      *         <tt>null</tt> if the cache contains no mapping for this key.
      * @throws ClassCastException
-     *             if the key is of an inappropriate type for this cache
-     *             (optional).
+     *             if the key is of an inappropriate type for this cache (optional).
      * @throws NullPointerException
      *             if the specified key is <tt>null</tt>
      * @throws UnsupportedOperationException
      *             if the cache does not cache entries
      */
-	CacheEntry<K, V> getEntry(K key);
+    CacheEntry<K, V> getEntry(K key);
 
-	/**
-     * Returns whether or not this cache contains a service of the specified
-     * type.
+    /**
+     * Returns the name of the cache. If no name has been specified while constructing the
+     * cache a random name should be generated.
      * 
-     * @param serviceType
-     *            the type of service
-     * @return true if this has a service of the specified type registered,
-     *         otherwise false
+     * @return the name of the cache
      */
-	boolean hasService(Class serviceType);
+    String getName();
 
-	/**
+    /**
      * Returns a service of the specified type.
      * 
      * @param <T>
@@ -186,59 +168,64 @@ public interface Cache<K, V> extends ConcurrentMap<K, V> {
      *             if no service of the specified type is registered
      * @see org.coconut.cache.service.CacheServices
      */
-	<T> T getService(Class<T> serviceType);
+    <T> T getService(Class<T> serviceType);
 
-	/**
-     * This method works analogoes to the {@link java.util.Map#get(Object)}
-     * method. However, it will not try to fetch missing items, it will only
-     * return a value if it actually exists in the cache. Furthermore, it will
-     * not effect the statistics gathered by the cache and no
-     * {@link CacheItemEvent.ItemAccessed} event will be raised. Finally, even
-     * if the item is expired it will still be returned.
+    /**
+     * Returns whether or not this cache contains a service of the specified type.
+     * 
+     * @param serviceType
+     *            the type of service
+     * @return true if this has a service of the specified type registered, otherwise
+     *         false
+     */
+    boolean hasService(Class<?> serviceType);
+
+    /**
+     * This method works analogoes to the {@link java.util.Map#get(Object)} method.
+     * However, it will not try to fetch missing items, it will only return a value if it
+     * actually exists in the cache. Furthermore, it will not effect the statistics
+     * gathered by the cache and no {@link CacheItemEvent.ItemAccessed} event will be
+     * raised. Finally, even if the item is expired it will still be returned.
      * <p>
-     * All implementations of this method should take care to assure that a call
-     * to peek does not have any side effects on the cache or any retrived
-     * value.
+     * All implementations of this method should take care to assure that a call to peek
+     * does not have any side effects on the cache or any retrived value.
      * 
      * @param key
      *            key whose associated value is to be returned.
-     * @return the value to which this cache maps the specified key, or
-     *         <tt>null</tt> if the cache contains no mapping for this key.
+     * @return the value to which this cache maps the specified key, or <tt>null</tt> if
+     *         the cache contains no mapping for this key.
      * @throws ClassCastException
-     *             if the key is of an inappropriate type for this cache
-     *             (optional).
+     *             if the key is of an inappropriate type for this cache (optional).
      * @throws NullPointerException
      *             if the specified key is <tt>null</tt>
      */
-	V peek(K key);
+    V peek(K key);
 
-	/**
-     * This method works analogoes to the {@link #peek(Object)} method. However,
-     * it will return a cache entry instead of just the value.
+    /**
+     * This method works analogoes to the {@link #peek(Object)} method. However, it will
+     * return a cache entry instead of just the value.
      * <p>
-     * Just like {@link #peek(Object)} any implementation of this method should
-     * take care to assure that the call does not have any side effects on the
-     * cache or any retrived entry.
+     * Just like {@link #peek(Object)} any implementation of this method should take care
+     * to assure that the call does not have any side effects on the cache or any retrived
+     * entry.
      * 
      * @param key
      *            key whose associated value is to be returned.
      * @return the cache entry to which this cache maps the specified key, or
      *         <tt>null</tt> if the cache contains no mapping for this key.
      * @throws ClassCastException
-     *             if the key is of an inappropriate type for this cache
-     *             (optional).
+     *             if the key is of an inappropriate type for this cache (optional).
      * @throws NullPointerException
      *             if the specified key is <tt>null</tt>
      */
-	CacheEntry<K, V> peekEntry(K key);
+    CacheEntry<K, V> peekEntry(K key);
 
-	/**
-     * Associates the specified value with the specified key in this cache
-     * (optional operation). If the cache previously contained a mapping for
-     * this key, the old value is replaced by the specified value. (A cache
-     * <tt>m</tt> is said to contain a mapping for a key <tt>k</tt> if and
-     * only if {@link #containsKey(Object) m.containsKey(k)} would return
-     * <tt>true</tt>.))
+    /**
+     * Associates the specified value with the specified key in this cache (optional
+     * operation). If the cache previously contained a mapping for this key, the old value
+     * is replaced by the specified value. (A cache <tt>m</tt> is said to contain a
+     * mapping for a key <tt>k</tt> if and only if
+     * {@link #containsKey(Object) m.containsKey(k)} would return <tt>true</tt>.))
      * 
      * @param key
      *            key with which the specified value is to be associated.
@@ -246,20 +233,19 @@ public interface Cache<K, V> extends ConcurrentMap<K, V> {
      *            value to be associated with the specified key.
      * @param attributes
      *            a map of additional attributes
-     * @return previous value associated with specified key, or <tt>null</tt>
-     *         if there was no mapping for key.
+     * @return previous value associated with specified key, or <tt>null</tt> if there
+     *         was no mapping for key.
      * @throws UnsupportedOperationException
-     *             if the <tt>put</tt> operation is not supported by this
-     *             cache.
+     *             if the <tt>put</tt> operation is not supported by this cache.
      * @throws ClassCastException
-     *             if the class of the specified key or value prevents it from
-     *             being stored in this cache.
-     * @throws IllegalArgumentException
-     *             if some aspect of this key or value prevents it from being
+     *             if the class of the specified key or value prevents it from being
      *             stored in this cache.
+     * @throws IllegalArgumentException
+     *             if some aspect of this key or value prevents it from being stored in
+     *             this cache.
      * @throws NullPointerException
      *             if specified key or value is <tt>null</tt>.
      * @see Map#put(Object, Object)
      */
-	V put(K key, V value, AttributeMap attributes);
+    V put(K key, V value, AttributeMap attributes);
 }
