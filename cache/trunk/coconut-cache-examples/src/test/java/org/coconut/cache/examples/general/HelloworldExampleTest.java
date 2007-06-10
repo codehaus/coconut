9a92c1b7314091760ae0b4d4e8208f8d47f6d719
kasper <kasper@0b154b62-a015-0410-9c24-a35e082c6f94>
2007-06-10 19:29:37 +0000

    adding example test

---
+++ cache/trunk/coconut-cache-examples/src/test/java/org/coconut/cache/examples/general/HelloworldExampleTest.java
@@ -0,0 +1,28 @@
+package org.coconut.cache.examples.general;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloworldExampleTest {
+    @Test
+    public void testHelloWorld() {
+        MyOutput o = new MyOutput();
+        PrintStream old = System.out;
+        System.setOut(new PrintStream(o));
+        HelloworldExample.main(new String[0]);
+        System.setOut(old);
+        assertTrue(o.buf.toString().startsWith("Hello world!"));
+    }
+
+    private static class MyOutput extends OutputStream {
+        StringBuffer buf = new StringBuffer();
+
+        public void write(int b) throws IOException {
+            buf.append((char) b);
+        }
+    }
+}
