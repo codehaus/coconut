a811739222f93175bccd62f2de671b8ea0eacfce
kasper <kasper@0b154b62-a015-0410-9c24-a35e082c6f94>
2007-10-24 11:32:16 +0000

    adding abstract worker service

---
+++ cache/trunk/coconut-cache-impl/src/main/java/org/coconut/cache/internal/service/worker/AbstractCacheWorkerService.java
@@ -0,0 +1,29 @@
+package org.coconut.cache.internal.service.worker;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ScheduledExecutorService;
+
+import org.coconut.cache.service.servicemanager.AbstractCacheLifecycle;
+import org.coconut.core.AttributeMaps;
+
+public abstract class AbstractCacheWorkerService extends AbstractCacheLifecycle implements
+        CacheWorkerService {
+
+    /**
+     * Returns a ExecutorService that can be used to asynchronously execute tasks for the
+     * specified service.
+     * 
+     * @param service
+     *            the service to return an ExecutorService for
+     * @return a ExecutorService that can be used to asynchronously execute tasks for the
+     *         specified service
+     */
+    public final ExecutorService getExecutorService(Class<?> service) {
+        return getManager().getExecutorService(service, AttributeMaps.EMPTY_MAP);
+    }
+
+    public ScheduledExecutorService getScheduledExecutorService(Class<?> service) {
+        return getManager().getScheduledExecutorService(service, AttributeMaps.EMPTY_MAP);
+    }
+
+}
