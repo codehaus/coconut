387c31ea956c4741ef07d6c27d7abb8c2f4e3d3e
kasper <kasper@0b154b62-a015-0410-9c24-a35e082c6f94>
2007-11-15 13:06:33 +0000

    updating example

--- cache/trunk/coconut-cache-examples/src/main/java/org/coconut/cache/examples/loading/SimpleUrlLoaderUsage.java
+++ cache/trunk/coconut-cache-examples/src/main/java/org/coconut/cache/examples/loading/SimpleUrlLoaderUsage.java
@@ -1,17 +1,17 @@
 package org.coconut.cache.examples.loading;
 
+// START SNIPPET: class
 import org.coconut.cache.Cache;
 import org.coconut.cache.CacheConfiguration;
-import org.coconut.cache.defaults.UnsynchronizedCache;
+import org.coconut.cache.defaults.SynchronizedCache;
 
 public class SimpleUrlLoaderUsage {
-    // START SNIPPET: class
     public static void main(String[] args) {
         CacheConfiguration<String, String> conf = CacheConfiguration.create();
         conf.loading().setLoader(new SimpleUrlLoader());
-        Cache<String, String> cache = conf.newCacheInstance(UnsynchronizedCache.class);
+        Cache<String, String> cache = conf.newCacheInstance(SynchronizedCache.class);
         System.out.println(cache.get("http://www.google.com"));// uses CacheLoader
         System.out.println(cache.get("http://www.google.com"));// uses cached version
     }
-    // END SNIPPET: class
 }
+// END SNIPPET: class
