9e59fc2e7b7af73e9bfbadcf64b1cb0836b760c8
kasper <kasper@0b154b62-a015-0410-9c24-a35e082c6f94>
2007-11-16 17:12:34 +0000

    adding examples

---
+++ cache/trunk/coconut-cache-examples/src/main/java/org/coconut/cache/examples/management/EnableManagementCustomDomain.java
@@ -0,0 +1,20 @@
+/* Written by Kasper Nielsen and released to the public domain, as explained at
+ * http://creativecommons.org/licenses/publicdomain
+ */
+package org.coconut.cache.examples.management;
+
+//START SNIPPET: class
+import org.coconut.cache.Cache;
+import org.coconut.cache.CacheConfiguration;
+import org.coconut.cache.defaults.SynchronizedCache;
+
+public class EnableManagementCustomDomain {
+    public static void main(String[] args) throws InterruptedException {
+        CacheConfiguration<String, String> conf = CacheConfiguration.create("ManagementTest");
+        conf.management().setEnabled(true).setDomain("com.acme");
+        Cache<String, String> cache = conf.newCacheInstance(SynchronizedCache.class);
+        cache.put("hello", "world");
+        Thread.sleep(10 * 60 * 1000); // sleep 10 minutes
+    }
+}
+//END SNIPPET: class
