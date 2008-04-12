765740acdcf8baf7112a09bdc1a4ad57b3e26891
kasper <kasper@0b154b62-a015-0410-9c24-a35e082c6f94>
2008-04-12 09:06:11 +0000

    removing management

--- cake/trunk/cake-core/src/test/java/org/codehaus/cake/management/stubs/PrivateMethods.java
+++
@@ -1,9 +0,0 @@
-/* Copyright 2004 - 2008 Kasper Nielsen <kasper@codehaus.org>
- * Licensed under the Apache 2.0 License. */
-package org.codehaus.cake.management.stubs;
-
-public class PrivateMethods {
-    private String getIllegal() {return null;};
-    private void setIllegal(String parameter) {};
-    private void illegal() {};
-}
